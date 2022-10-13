a673781906850c0a3c84e4d24ae9dae101d898c3
tidligere <103850229+supremacy74@users.noreply.github.com>
2022-10-13 19:39:30 +0600

    fourth

--- index.js
+++ index.js
@@ -10,20 +10,3 @@ document.querySelectorAll('.examples__block').forEach((item, index) => {
         })
     })
 })
-
-const next = document.querySelector('.next')
-const prev = document.querySelector('.prev')
-
-let scroll = 0
-
-const wrapper = document.querySelector('.description__wrapper')
-
-next.addEventListener('click', () => {
-    scroll < 200 ? scroll += 100 : 0
-    wrapper.style.transform = `translateX(-${scroll}vw)`
-})
-
-prev.addEventListener('click', () => {
-    scroll > 0 ? scroll -= 100 : 200
-    wrapper.style.transform = `translateX(-${scroll}vw)`
-})
